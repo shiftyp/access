91217f1b0cfcdd74d673591caba154700d2b2f49
ryan <ryan@rkahn.dev>
2025-01-07 19:12:04 +0000

    Changed the render element for the book list. Added search to booklist

--- app.ts
+++ app.ts
@@ -15,9 +15,19 @@ const Books = (books: typeof bookData) => {
 }
 
 const render = (books: typeof bookData, query: string = '') => {
-    document.body.innerHTML = Books(books.filter(book => {
+    document.getElementById('results')!.innerHTML = Books(books.filter(book => {
         return book.title.toLowerCase().includes(query.toLowerCase())
     }));
 }
+
+document.getElementById('search-input')?.addEventListener('keyup', (event) => {
+    const query = (event.target as HTMLInputElement).value
+
+    render(bookData, query)
+})
+
+document.getElementById('search-form')?.addEventListener('submit', (event) => {
+    event.preventDefault()
+})
 
 render(bookData)
